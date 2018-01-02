6d6d15fb3dfd4a6342a9d6cfdebd68413b405ccd
Claudiagari <claudia.garibotto@gmail.com>
2018-01-01 23:15:20 -0500

    comentando index - js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Iniciando la pagina para pasar a la principal
 $(document).ready(function() {
   $('.loading').delay(3000).fadeOut(500, function() {
     window.location.assign('views/mainpage.html');
